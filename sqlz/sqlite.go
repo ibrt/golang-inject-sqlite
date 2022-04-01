8f0ffd2aa64aacc9c1fa2f3891e5f794d6c537cb
Ivan Bertona <ivan@ibrt.me>
2022-04-01 15:37:20 -0400

    Make SQLite implment boil.ContextExecutor.

--- sqlz/sqlite.go
+++ sqlz/sqlite.go
@@ -104,6 +104,9 @@ func GetConfig(ctx context.Context) *Config {
 
 // SQLite describes the sqlite module (a subset of *sql.DB).
 type SQLite interface {
+	Exec(query string, args ...interface{}) (sql.Result, error)
+	Query(query string, args ...interface{}) (*sql.Rows, error)
+	QueryRow(query string, args ...interface{}) *sql.Row
 	ExecContext(ctx context.Context, query string, args ...interface{}) (sql.Result, error)
 	QueryContext(ctx context.Context, query string, args ...interface{}) (*sql.Rows, error)
 	QueryRowContext(ctx context.Context, query string, args ...interface{}) *sql.Row
